9858e8a680fd94b1fe719c16a233e9afe89fa181
Kiswari10 <kiswari10@gmail.com>
2020-05-20 11:42:51 -0500

    modifica server y webhook

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
 const config = require('./config');
+const middleware =require("./middleware");
 
 const { port } = config;
 
 app.use(express.json());
 
-app.post('/webhook', (req, res) => {
-    const dato = req.body
-    console.log(dato)
-    res.json({mensaje:"Hola desde el server"})
+app.post("/webhook", (req, res) => {
+    const {arrOfNumber} = req.body
+    /* console.log(arrOfNumber) */
+    let condition = [];
+    let result = [];
+    arrOfNumber.forEach((number) => {
+        if(number %2 === 0){
+            condition = { Server: "Recibí un número par"}
+        } else {
+            condition = { Server: "Recibí un número impar"}
+        }
+        result.push(condition)
+        console.log( {Webhook : `{"mensaje":"${number}"}`})
+    })
+    /* console.log('result', result) */
+    res.json(result)
 })
 
 app.listen(port, () => {
--- webhook.js
+++ webhook.js
@@ -1,23 +1,31 @@
 const request = require("request");
 const url = "http://localhost:3200/webhook"
 
+let arrPares = []
+for(let i =0; i< 20; i+=2 ) {
+    arrPares.push(i)
+}
+let arrImpares = []
+for(let i =1; i< 20; i+=2 ) {
+    arrImpares.push(i)
+}
+const arrOfNumber = [...arrPares, ...arrImpares]
 setInterval(() => {
-    let dato ={
-        id: Math.round(Math.random()*10),
-        mensaje:"Hola desde el webhook"
-    }
+    let data ={ arrOfNumber }
     request({
         method: 'POST',
         uri: url,
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(dato)
+        body: JSON.stringify(data)
     }, (error, response, body) => {
         if (error) {
             console.log("no se pudo enviar el mensaje")
         }
-        let mensaje =JSON.parse(body)
-        console.log(mensaje);
+        let mensajes =JSON.parse(body)
+        mensajes.forEach((mensaje) => {
+            console.log(mensaje);
+        })
     })
 }, 5000)
