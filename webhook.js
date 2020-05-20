9ec30aca9478f9fdaad39459714d54b7a5bcfc00
Kiswari10 <kiswari10@gmail.com>
2020-05-20 16:19:49 -0500

    agrega iteradores y condiciones en webhook.js

--- webhook.js
+++ webhook.js
@@ -1,16 +1,44 @@
 const request = require("request");
 const url = "http://localhost:3200/webhook"
 
+/* 
 let arrPares = []
-for(let i =0; i< 20; i+=2 ) {
-    arrPares.push(i)
-}
-let arrImpares = []
-for(let i =1; i< 20; i+=2 ) {
-    arrImpares.push(i)
-}
-const arrOfNumber = [...arrPares, ...arrImpares]
+    let arrImpares = []
+    for(let i = iterator; i< limit; i++ ) {
+        if(i %2 === 0){
+            arrPares.push(i)
+        } else {
+            arrImpares.push(i)
+        }
+    }
+    const arrOfNumber = [...arrPares, ...arrImpares]
+    iterator += 20
+    limit += 20;
+    let data ={ arrOfNumber }
+*/
+let limit = 20
+let iterator = 0;
+let condition = false
 setInterval(() => {
+    let arrPares = []
+    let arrImpares = []
+    let arrOfNumber = []
+    for(let i = iterator; i< limit; i++ ) {
+        if(i %2 === 0){
+            arrPares.push(i)
+        } else {
+            arrImpares.push(i)
+        }
+    }
+    if(condition) {
+        arrOfNumber = arrImpares
+        iterator += 20
+        limit += 20;
+        condition = !condition;
+    } else {
+        arrOfNumber = arrPares
+        condition = !condition;
+    }
     let data ={ arrOfNumber }
     request({
         method: 'POST',
